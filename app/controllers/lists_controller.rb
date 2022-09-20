9e85700f421a6d9d492f5746298a4b6b502ddea0
Maria Antonieta Pereira <antonietap0@gmail.com>
2022-09-19 22:29:27 -0500

    change route list_path

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -16,7 +16,7 @@ class ListsController < ApplicationController
     @list = List.create(list_params)
     @list.save
       if @list.save
-        redirect_to lists_path
+        redirect_to root_path
       else
         render :new, status: :unprocessable_entity
       end
